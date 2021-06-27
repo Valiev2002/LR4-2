a4f560f95ccafda862f97317b2157e992623c1a1
Valiev2002 <70635274+Valiev2002@users.noreply.github.com>
2021-06-27 21:10:26 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<stdexcept>
 #include <algorithm>
+#include<exception>
 #include <string>
 class my_ex: public std::exception{
 public:
@@ -11,6 +12,7 @@ public:
     int get_data(){//возвращает нашу дату
         return data;
     }
+     virtual const char* what() { return std::exception::what();};
 private:
     int data;
 };
@@ -23,33 +25,34 @@ void foo(int age){
         throw my_ex("What?",age);
     }
 }
-class King { virtual ~King() {} };
-class Prince { virtual ~Prince() {} };
+class King { virtual ~King();};
+class Prince { virtual ~Prince(); };
 void foo1(int a){
     std::cout<<"Enter a>0"<<std::endl;
+    std::cin>>a;
     if(a<0){
-        throw std::exception();
+        throw std::invalid_argument("///");
     }
-    else{std::cout<<"a = "<<std::endl;}
+    else{std::cout<<"a = "<<a<<std::endl;}
 }
 class A {
 private:
     int *ptr;
 public:
     A() {
-        int *ptr = new int[10];
+         ptr = new int[10];
     }
     ~A() {
-        delete[] ptr;
+        delete [] ptr;
     }
 };
 int main() {
-    std::cout<<"First exception"<<std::endl;
+   /*std::cout<<"First exception"<<std::endl;
     std::vector<int> vec(5);
     std::cout << "Vector size = " << vec.size() << '\n';
     try {
         if(vec.size()>5) {
-            throw 1;
+            throw std::exception();
         }
             vec.at(6) = 2002;//СЕТИМ 6 ЭЛЕМЕНТ,КОТОРОГО НЕТ
     }
@@ -57,23 +60,20 @@ int main() {
         std::cout << "Error: there are 5 elements in vector "<<std::endl<<e.what() <<'\n';
     }
     //-----------------------------------------------------------------------------------
-    std::cout<<"Second exception"<<std::endl;
-      try {
+    */std::cout<<"Second exception"<<std::endl;
+    try {
         std::vector<int> myvector(30);
-        int new_size;
+        int new_size=0;
         std::cout<<"Vector size is "<<myvector.size()<<std::endl;
         std::cin>>new_size;
-            myvector.reserve(myvector.size()+1);
-        if(new_size>myvector.size())
-        {
-            throw 1;
+        myvector.reserve(-1);
+            if (new_size > myvector.max_size()) {
+                throw std::length_error("---");
+            }
         }
-    }
-    catch (int new_size) {
-    std::cout<< "Length error " << '\n';
-}
 
-  std::cout<<"Third exception"<<std::endl;
+    catch(std::length_error&le){std::cout<< "Argument error: " << le.what() << '\n';}
+  /*std::cout<<"Third exception"<<std::endl;
     try{
         std::string str1 = "Valiev Vali Samedovich";
         std::string str2 = "Student of BMSTU";
@@ -83,6 +83,14 @@ int main() {
         std::cout << "std::stoi(\"" << str2 << "\") is " << my_int2 << '\n';
         }
     catch(std::invalid_argument&inv){std::cout<< "Argument error: " << inv.what() << '\n';}
+    std::cout<<"Fourth exception"<<std::endl;
+    Prince p;
+    try {
+        King& k = dynamic_cast<King&>(p);
+    } catch(const std::bad_cast& e)
+    {
+        std::cout << e.what() << '\n';
+    }
         std::cout<<"Fifth exception"<<std::endl;
     long double *ptr ;
 
@@ -95,7 +103,7 @@ int main() {
     catch(std::bad_alloc& e)
     {
         std::cout << e.what() << std::endl;
-    }
+    }*/
     std::cout<<"Task 2"<<std::endl;
     try{
         foo(-1);
@@ -105,39 +113,30 @@ int main() {
         std::cout<<"Data status: "<<ex.get_data()<<std::endl<<"This is where something went wrong,"
                                                               "because age can't be with the minus"<<std::endl;
     }
-
-    std::cout<<"Fourth exception"<<std::endl;
-    Prince p;
-    try {
-        King& k = dynamic_cast<King&>(p);
-    } catch(const std::bad_cast& e)
-    {
-        std::cout << e.what() << '\n';
-    }
   std::cout<<"Task 3"<<std::endl;
     try
     {
         int *a=new int [10];
-        foo1(0);
+        foo1(-1);
         delete[] a;
     }
     catch(...)
     {
-        std::cout << "Error";
+        std::cout<< "Error";
     }
-    std::cout<<"It is incorrect code,because we delete only pointer,but not an array"<<std::endl;
+    std::cout<<"UTECHKA"<<std::endl;
     std::cout<<"Now let's see correct code"<<std::endl;
-    int* a = nullptr;
     try
     {
-        a = new int[10];
-        foo1(0);
+        A b;
+        int *a = new int[10];
+        foo1(-1);
+        delete[] a;
     }
     catch(...)
     {
         std::cout << "Error\n";
     }
-    delete[] a;
     return 0;
 }
 
